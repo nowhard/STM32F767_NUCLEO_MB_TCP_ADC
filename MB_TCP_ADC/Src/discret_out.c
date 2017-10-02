b36b630f766f6dbff0226368ea24a4b62a800140
nowhard <nowhard@mail.ru>
2017-10-02 14:11:10 +0300

    При запуске последовательности теперь не создается новая задача(проблема с нехваткой памяти FreeRTOS)

--- MB_TCP_ADC/Src/discret_out.c
+++ MB_TCP_ADC/Src/discret_out.c
@@ -4,6 +4,7 @@
 
 #include "FreeRTOS.h"
 #include "task.h"
+#include "semphr.h"
 
 #include "stm32f7xx_hal.h"
 #include "stm32f7xx.h"
@@ -34,7 +35,7 @@ stSetSequenceParams	discrOutSequenceParams={OUTPUTS_REG_ALL_RELAY_OFF,OUTPUTS_RE
 extern SPI_HandleTypeDef hspi5;
 extern DMA_HandleTypeDef hdma_spi5_tx;
 
-//enDiscrOutTransferState DiscrOutTransferState=DISCR_OUT_TRANSFER_COMPLETE;
+xSemaphoreHandle xOutSequenceSem;
 
 void SPI5_DMA_TransferCallback(void);
 void DiscretOutputs_Enable(enDiscrOutState DiscrOutState);
@@ -47,13 +48,16 @@ void DiscretOutputs_SetSequence_Task( void *pvParameters );
 
 void DiscretOutputs_Init(void)
 {
+		xOutSequenceSem=xSemaphoreCreateBinary();
+	
 		DiscretOutputs_Enable(DISCR_OUT_ENABLE);
 		HAL_GPIO_WritePin(STROB_GPIO_Port, STROB_Pin, GPIO_PIN_RESET);
 		
 		DiscretOutputs_Set(OUTPUTS_REG_ALL_RELAY_OFF);
 		HAL_GPIO_WritePin(ENABLE_OUT_1_GPIO_Port, ENABLE_OUT_1_Pin, GPIO_PIN_RESET);//включаем оптроны реле
 		HAL_GPIO_WritePin(ENABLE_OUT_7_GPIO_Port, ENABLE_OUT_7_Pin, GPIO_PIN_RESET);
-//		HAL_DMA_RegisterCallback(&hdma_spi5_tx,HAL_DMA_XFER_CPLT_CB_ID,SPI5_DMA_TransferCallback);
+
+		xTaskCreate( DiscretOutputs_SetSequence_Task, "Set Sequence  Task", SEQUENCE_SET_TASK_STACK_SIZE, (void *)&discrOutSequenceParams, SEQUENCE_SET_TASK_PRIO, NULL );
 }
 
 void DiscretOutputs_Enable(enDiscrOutState DiscrOutState)
@@ -84,7 +88,7 @@ uint8_t DiscretOutputs_StartSequence(void)
 {
 	if(!discrOutSequenceProgress)
 	{
-			xTaskCreate( DiscretOutputs_SetSequence_Task, "Set Sequence  Task", SEQUENCE_SET_TASK_STACK_SIZE, (void *)&discrOutSequenceParams, SEQUENCE_SET_TASK_PRIO, NULL );
+			xSemaphoreGive(xOutSequenceSem);
 	}
 }
 
@@ -94,26 +98,30 @@ void DiscretOutputs_SetSequence_Task( void *pvParameters )
 	uint16_t i=0;
 	stSetSequenceParams *taskParams;
 
-	
-	discrOutSequenceProgress=1;
-	taskParams=(stSetSequenceParams*)pvParameters;
-
-	if(IS_DISCR_OUT_TIME(taskParams->time) && IS_DISCR_OUT_NUM_CYCLES(taskParams->num_cycles))
+	while(1)
 	{
-			for(i=0;i<taskParams->num_cycles;i++)
+			xSemaphoreTake(xOutSequenceSem,portMAX_DELAY);
+			
+			discrOutSequenceProgress=1;
+			taskParams=(stSetSequenceParams*)pvParameters;
+
+			if(IS_DISCR_OUT_TIME(taskParams->time) && IS_DISCR_OUT_NUM_CYCLES(taskParams->num_cycles))
 			{
-				discrOutTempReg=taskParams->state_1;
-				DiscretOutputs_Set(taskParams->state_1);
-				vTaskDelay(taskParams->time);
-				discrOutTempReg=taskParams->state_2;
-				DiscretOutputs_Set(taskParams->state_2);
-				vTaskDelay(taskParams->time);
+					for(i=0;i<taskParams->num_cycles;i++)
+					{
+						discrOutTempReg=taskParams->state_1;
+						DiscretOutputs_Set(taskParams->state_1);
+						vTaskDelay(taskParams->time);
+						discrOutTempReg=taskParams->state_2;
+						DiscretOutputs_Set(taskParams->state_2);
+						vTaskDelay(taskParams->time);
+					}
+					discrOutTempReg=taskParams->state_end;
+					DiscretOutputs_Set(taskParams->state_end);
 			}
-			discrOutTempReg=taskParams->state_end;
-			DiscretOutputs_Set(taskParams->state_end);
+			discrOutSequenceProgress=0;
 	}
-	discrOutSequenceProgress=0;
-	vTaskDelete(NULL);
+
 }
 
 
